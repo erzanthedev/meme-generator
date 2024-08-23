c7d871a1938e835fac6232af21ed4af44e0b10f5
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-23 16:26:15 +1000

    feat: added logic to fetch memes from API

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,4 +1,4 @@
-import { useId, useState } from "react";
+import { useId, useState, useEffect } from "react";
 import sampleMeme from "../assets/sample-meme.png";
 
 export default function MainContent() {
@@ -6,10 +6,22 @@ export default function MainContent() {
     topText: "",
     bottomText: "",
   });
+  const [allMemes, setAllMemes] = useState([]);
 
-  console.log(formData);
+  useEffect(() => {
+    async function getMemeImages() {
+      try {
+        const response = await fetch("https://api.imgflip.com/get_memes");
+        const results = await response.json();
+        setAllMemes(results.data.memes);
+      } catch (err) {
+        // Handle error
+        console.log("Error:", err);
+      }
+    }
 
-  const id = useId();
+    getMemeImages();
+  }, []);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -19,9 +31,11 @@ export default function MainContent() {
     }));
   }
 
+  const id = useId();
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <div className="form-input-wrapper">
           <label className="form-label" htmlFor={id + "-topText"}>
             Top Text
@@ -49,7 +63,7 @@ export default function MainContent() {
           />
         </div>
         <button className="form-btn">Get a new meme image 🖼</button>
-      </form>
+      </div>
       <div className="meme-container">
         <img className="meme-image" src={sampleMeme} alt="sampleMeme" />
         <h2 className="meme-text top">{formData.topText}</h2>
