c0504d362937827946b8d06ddb7eb897809ce6fd
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-23 14:30:50 +1000

    feat: added styling form to align

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,3 +1,20 @@
+import { useId } from "react";
+
 export default function MainContent() {
-  return <h1>MainContent</h1>;
+  const id = useId();
+  return (
+    <main>
+      <form className="form">
+        <div className="form-input-wrapper">
+          <label htmlFor={id + "-topText"}>Top Text</label>
+          <input type="text" id={id + "-topText"} />
+        </div>
+        <div className="form-input-wrapper">
+          <label htmlFor={id + "-bottomText"}>Bottom Text</label>
+          <input type="text" id={id + "-bottomText"} />
+        </div>
+        <button className="form-btn">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
 }
