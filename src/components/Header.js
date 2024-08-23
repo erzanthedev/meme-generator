124fe0cddab73148bd35848c29b97592b6179bbc
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-23 11:51:49 +1000

    feat: completed Header component and added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,11 @@
+import trollFace from "../assets/troll-face.png";
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <nav>
+      <img className="header-logo" src={trollFace} alt="" />
+      <h2 className="header-title">Meme Generator</h2>
+      <h3 className="header-text">React Course - Project 3</h3>
+    </nav>
+  );
 }
